bf56888605e1899fbf3bceee0f8116f0fdd59491
mizutombo <thomas.shultz52@gmail.com>
2016-10-24 16:53:53 -0700

    added fake test for pass

---
+++ test.js
@@ -0,0 +1,8 @@
+// This assignment does not require tests.
+// Therefore, to satisfy npm test requirements for mocha, created fake test.
+const assert = require('chai').assert;
+
+it('runs a fake test', done => {
+  assert('this assignment does not require tests', true);
+  done();
+});
