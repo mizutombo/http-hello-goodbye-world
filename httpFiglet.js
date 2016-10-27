de15de5ed4d329811d433fb85960def804842d64
mizutombo <thomas.shultz52@gmail.com>
2016-10-27 08:32:17 -0700

    initial code added for Lab2

---
+++ httpFiglet.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const fs = require('fs');
+const figlet = require('figlet');
+const indexHtml = fs.createReadStream('index.html');
+
+// create new instance of http server
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  res.statusCode = 200; // http status code 200 = okay
+  if (req.url === '/') {
+    indexHtml.pipe(res);
+  }
+  if (req.url === '/halloween') {
+    figlet.text('Boo!', {
+      font: 'Ghost',
+      horizontalLayout: 'default',
+      verticalLayout: 'default'
+    }, function(err, data) {
+      if (err) {
+        console.log('400 - gremlins');
+        console.dir(err);
+        return;
+      }
+      console.log(data);
+    });
+  }
+});
+
+const port = 8080;
+server.listen(port, err => {
+  if(err) console('error', err);
+  else console.log('http server listening on port', port);
+});
